4c2a7620f4a65cf8e7ec06fdf60422b90ff7e987
pancanin <valericfbg@gmail.com>
2025-04-07 22:43:13 +0300

    WIP on the json parser of scenes

--- input/SceneDecoderJSON.cpp
+++ input/SceneDecoderJSON.cpp
@@ -1,9 +1,18 @@
 #include "SceneDecoderJSON.h"
 
 #include <assert.h>
+#include <format>
 
 #include "rapidjson/document.h"
 #include "rapidjson/error/en.h"
+
+constexpr char missingKeyFmt[] = "Missing or invalid type for key '{}'";
+constexpr char sizeMismatchFmt[] = "Unexpected element count of {} when {} is required for key '{}'";
+constexpr char kSettings[] = "settings";
+constexpr char kBgColor[] = "background_color";
+constexpr char kImage[] = "image";
+constexpr char kWidth[] = "width";
+constexpr char kHeight[] = "height";
 
 std::expected<Scene, std::string> SceneDecoderJSON::decode(const uint8_t* data, size_t len)
 {
@@ -21,8 +30,49 @@ std::expected<Scene, std::string> SceneDecoderJSON::decode(const uint8_t* data, 
   }
 
   if (!d.IsObject()) {
-    return std::unexpected("JSON: Expected object at the root.");
+    return std::unexpected("Expected object at the root.");
   }
 
-  // TODO: Mahni si .vs ot git
+  const auto& settings = d[kSettings];
+
+  if (!settings.IsObject()) {
+    return std::unexpected(std::format(missingKeyFmt, kSettings));
+  }
+
+  const auto& bgColor = settings[kBgColor];
+
+  if (!bgColor.IsArray()) {
+    return std::unexpected(std::format(missingKeyFmt, kBgColor));
+  }
+
+  constexpr int bgColorSize = 3;
+  if (bgColor.Size() != bgColorSize) {
+    return std::unexpected(std::format(sizeMismatchFmt, bgColor.Size(), bgColorSize, kBgColor));
+  }
+
+  // TODO: set in Scene
+
+  const auto& image = settings[kImage];
+
+  if (!image.IsObject()) {
+    return std::unexpected(std::format(missingKeyFmt, kImage));
+  }
+
+  const auto& imageWidth = image[kWidth];
+
+  if (!imageWidth.IsNumber()) {
+    return std::unexpected(std::format(missingKeyFmt, std::format("{}->{}", kImage, kWidth)));
+  }
+
+  // TODO: the width is ready
+
+  const auto& imageHeight = image[kHeight];
+
+  if (!imageHeight.IsNumber()) {
+    return std::unexpected(std::format(missingKeyFmt, std::format("{}->{}", kImage, kHeight)));
+  }
+
+  // TODO: the height is ready too
+
+
 }
