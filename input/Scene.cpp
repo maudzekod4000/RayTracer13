fd56aebd2c9c27a4b38521aa81a21781b60c4f2e
pancanin <valericfbg@gmail.com>
2025-04-07 23:04:01 +0300

    defining classes for the scene objects

--- input/Scene.cpp
+++ input/Scene.cpp
@@ -4,3 +4,13 @@ Scene::Scene(std::vector<Triangle>&& t):
 	triangles(std::move(t))
 {
 }
+
+ImageSettings::ImageSettings(uint16_t w, uint16_t h):
+	width(w), height(h)
+{
+}
+
+uint16_t ImageSettings::getWidth() const noexcept
+{
+	return width;
+}
--- input/Scene.h
+++ input/Scene.h
@@ -2,8 +2,28 @@
 #define SCENE_H
 
 #include <vector>
+#include <cstdint>
 
 #include "Triangle.h"
+#include "TypeDefs.h"
+
+class ImageSettings final {
+public:
+	explicit ImageSettings(uint16_t w, uint16_t h) noexcept;
+
+	uint16_t getWidth() const noexcept;
+private:
+	uint16_t width;
+	uint16_t height;
+};
+
+class Settings {
+public:
+
+private:
+	// The color of the background when the ray goes to infinity.
+	Vec3 backgroundColor;
+};
 
 class Scene {
 public:
